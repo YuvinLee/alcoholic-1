42d0a8a82939804ff38298f781d560f245f3390e
San <san0558@naver.com>
2020-07-19 18:20:27 +0900

    QRCODE 생성과 인식 완료

--- alcoholic/app/src/main/java/com/techtown/alcoholic/Activity/RoomActivity.java
+++ alcoholic/app/src/main/java/com/techtown/alcoholic/Activity/RoomActivity.java
@@ -2,15 +2,51 @@ package com.techtown.alcoholic.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
 import com.techtown.alcoholic.R;
 
-public class RoomActivity extends AppCompatActivity {
+import java.util.Hashtable;
 
+public class RoomActivity extends AppCompatActivity {
+    private String TAG = "RoomLog";
+
+    //QR코드가 보여질 이미지 부분
+    private ImageView imageViewQRCode;
+    //QR코드 값 받기
+    private String textForQRCode;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_room);
+
+
+        imageViewQRCode = (ImageView)findViewById(R.id.qrCode);
+
+
+        textForQRCode = "테스트";
+
+        Log.d(TAG, "onCreate: ");
+        MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+        try{
+            /* Encode to utf-8 */
+            Hashtable hints = new Hashtable();
+            hints.put(EncodeHintType.CHARACTER_SET, "utf-8");
+
+            //width랑 height에서 QR코드 이미지 크기 조정
+            BitMatrix bitMatrix = multiFormatWriter.encode(textForQRCode, BarcodeFormat.QR_CODE,200,200, hints);
+            BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+            Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+            imageViewQRCode.setImageBitmap(bitmap);
+
+        }catch (Exception e){}
     }
 }
